d6867fce13c584820d010e6f166e5278d34a9f27
bllagoje <bllagoje@gmail.com>
2023-03-28 00:30:42 +0200

    Add DOM interaction

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,37 @@ class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLElement
     element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
 
+    // --------------------------------------------------
     constructor() {
         this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement
         this.hostElement = document.getElementById("app")! as HTMLElement
         
         let importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = "user-input"
+        // ----------------------------
+        this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement
+        // ----------------------------
+        // Configure
+        this.configure()
+        // Attach
         this.attach()
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+        
+    }
+
+    private configure() {
+        this.element.addEventListener("submit", this.submitHandler.bind(this))
     }
 
     private attach() {
