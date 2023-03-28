24ebc7f1e99b3d5424c4c852d9403ed1802f3f57
bllagoje <bllagoje@gmail.com>
2023-03-28 15:50:27 +0200

    Add rendering project list

--- src/app.ts
+++ src/app.ts
@@ -47,6 +47,35 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 }
 
 // --------------------------------------------------
+// ProjectList class
+class ProjectList {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLElement
+    element: HTMLElement
+
+    constructor(private type: "active" | "finished") {
+        this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement
+        this.hostElement = document.getElementById("app")! as HTMLElement
+        
+        let importedNode = document.importNode(this.templateElement.content, true)
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${this.type}-projects`
+        this.attach()
+        this.renderContent()
+    }
+
+    private renderContent() {
+        let listId = `${this.type}-projects-list`
+        this.element.querySelector("ul")!.id = listId
+        this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS"
+
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element)
+    }
+}
+
 // ProjectInput class
 class ProjectInput {
     templateElement: HTMLTemplateElement
@@ -124,4 +153,8 @@ class ProjectInput {
 }
 
 
+// --------------------------------------------------
 const prjInput = new ProjectInput()
+const activePrjList = new ProjectList("active")
+const finishedPrjList = new ProjectList("finished")
+
