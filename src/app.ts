835cfb1c89ebae85444350d41366d3ebd8039b25
bllagoje <bllagoje@gmail.com>
2023-03-29 15:10:00 +0200

    Add drag and drop implementation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Drag and drop interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void
+    dragEndHandler(event: DragEvent): void
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void
+    dropHandler(event: DragEvent): void
+    dragLeaveHandler(event: DragEvent): void
+}
+
 // Project type
 enum ProjectStatus { Active, Finished }
 
@@ -125,7 +137,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjectItem class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     private project: Project
 
     get persons() {
@@ -144,7 +156,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent()
     }
 
-    configure() {}
+    @autobind
+    dragStartHandler(event: DragEvent) {
+        
+    }
+
+    dragEndHandler(event: DragEvent) {
+        
+    }
+
+    configure() {
+        this.element.addEventListener("dragstart", this.dragStartHandler)
+        this.element.addEventListener("dragend", this.dragEndHandler)
+    }
 
     renderContent() {
         this.element.querySelector("h2")!.textContent = this.project.title
