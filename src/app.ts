9c87aacb4b93a598e9b2f966931b1e60ee975cd2
bllagoje <bllagoje@gmail.com>
2023-03-30 15:59:45 +0200

    Update imports

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
-// Drag and drop interfaces
-/// <reference path="models/drag-drop.ts" />
-// Project type
-/// <reference path="models/project.ts" />
-// Project state management
-/// <reference path="state/project-state.ts" />
-// Validation
-/// <reference path="util/validation.ts" />
-// Autobind decorator
-/// <reference path="decorators/autobind.ts" />
 // ProjectList
 /// <reference path="components/project-list.ts" />
 // ProjectInput
@@ -19,5 +9,3 @@ namespace App {
     new ProjectList("active")
     new ProjectList("finished")
 }    
-
-
--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,5 +1,8 @@
 // ProjectInput class
 /// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../util/validation.ts" />
+/// <reference path="../state/project-state.ts" />
 
 namespace App {
     export class ProjectInput extends Component<HTMLElement, HTMLFormElement> {
