02ff9efe571bbdcf55f26907ea50b2e8c19914f3
bllagoje <bllagoje@gmail.com>
2023-03-30 21:27:25 +0200

    Update webpack config for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,15 @@
-// Configs for development
+// Configs for production
 const path = require("path")
+const CleanPlugin = require("clean-webpack-plugin")
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: "./src/app.ts",
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "dist"
+        path: path.resolve(__dirname, "dist")
     },
-    devtool: "inline-source-map",
+    devtool: "none",
     module: {
         rules: [
             {
@@ -21,5 +21,8 @@ module.exports = {
     },
     resolve: {
         extensions: [".ts", ".js"]
-    }
+    },
+    plugins: [
+        new CleanPlugin.CleanWebpackPlugin()
+    ]
 }
